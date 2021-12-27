893793a82a0804f6abee70fa8b45794b8d2fb1a0
John Ockenden <29466887+ockendenjo@users.noreply.github.com>
2021-12-27 19:40:19 +0000

    Initial commit

---
+++ src/handlers/auth.js
@@ -0,0 +1,102 @@
+const https = require("https");
+const AWS = require("aws-sdk")
+
+const makeRequest = (path, payload, method = "POST", headers = {}) => new Promise((resolve, reject) => {
+    const options = {host: "strava.com", port: 443, path, method, headers};
+    const req = https.request(options, res => {
+        let buffer = "";
+        res.on("data", chunk => buffer += chunk);
+        res.on("end", () => {
+            resolve(JSON.parse(buffer))
+        });
+    });
+    req.on("error", e => reject(e.message));
+    req.write(JSON.stringify(payload));
+    req.end();
+});
+
+const http401 = {statusCode: 401, body: JSON.stringify("Unauthorized")};
+
+const ssm = new AWS.SSM();
+
+const getParams = () => {
+    return new Promise((r, x) => {
+        const params = {
+            Path: "/strava", Recursive: true, WithDecryption: true
+        };
+        ssm.getParametersByPath(params, function(err, data) {
+            if (err) {
+                x(err);
+            } else {
+                console.log(data);
+                const map = data.Parameters.reduce((acc, val) => {
+                    const key = val.Name.replace("/strava/", "");
+                    acc[key] = val.Value;
+                    return acc;
+                }, {});
+                r(map);
+            }
+        });
+    });
+};
+
+const setParam = (param, value, type = "String") => {
+    const data = {
+        Name: param,
+        Value: value,
+        Type: type
+    };
+
+    return new Promise((r, x) => {
+        ssm.putParameter(data, function(err, data) {
+            if (err) {
+                x(err);
+            } else {
+                r(data);
+            }
+        });
+    });
+};
+
+exports.handler = async(event) => {
+    console.log(event);
+
+    if (!event.queryStringParameters.code) {
+        return http401;
+    }
+
+    let params;
+    try {
+        params = await getParams();
+        console.log(params);
+    } catch (e) {
+        return http401;
+    }
+
+    const data = {
+        client_id: params.clientId,
+        client_secret: params.clientSecret,
+        code: event.queryStringParameters.code,
+        grant_type: "authorization_code"
+    };
+    const url = "https://www.strava.com/oauth/token?" + new URLSearchParams(data);
+
+    let res;
+    try {
+        res = await makeRequest(url, {});
+    } catch (e) {
+        console.error("Get token failed");
+        return http401;
+    }
+
+    if (res.athlete.id !== 21702111) {
+        return http401;
+    }
+
+    await setParam("/strava/refreshToken", res.refresh_token, "SecureString");
+    await setParam("/strava/accessToken", res.access_token, "SecureString");
+
+    return {
+        statusCode: 200, body: JSON.stringify("Authorized"),
+    };
+};
---
+++ src/handlers/check.js
@@ -0,0 +1,135 @@
+const https = require("https");
+const AWS = require("aws-sdk")
+
+const makeRequest = (path, payload, method = "POST", headers = {}) => new Promise((resolve, reject) => {
+    const options = {host: "strava.com", port: 443, path, method, headers};
+    const req = https.request(options, res => {
+        let buffer = "";
+        res.on("data", chunk => buffer += chunk);
+        res.on("end", () => {
+            resolve(JSON.parse(buffer))
+        });
+    });
+    req.on("error", e => reject(e.message));
+    req.write(JSON.stringify(payload));
+    req.end();
+});
+
+const ssm = new AWS.SSM();
+const sns = new AWS.SNS();
+
+const getParams = () => {
+    return new Promise((r, x) => {
+        const params = {
+            Path: "/strava", Recursive: true, WithDecryption: true
+        };
+        ssm.getParametersByPath(params, function(err, data) {
+            if (err) {
+                x(err);
+            } else {
+                const map = data.Parameters.reduce((acc, val) => {
+                    const key = val.Name.replace("/strava/", "");
+                    acc[key] = val.Value;
+                    return acc;
+                }, {});
+                r(map);
+            }
+        });
+    });
+};
+
+const setParam = (param, value, type = "String") => {
+    const data = {
+        Name: param, Value: value, Type: type, Overwrite: true
+    };
+
+    return new Promise((r, x) => {
+        ssm.putParameter(data, function(err, data) {
+            if (err) {
+                x(err);
+            } else {
+                r(data);
+            }
+        });
+    });
+};
+
+const publishSns = (params) => {
+    return new Promise((r,x) => {
+        sns.publish(params, function(err, data) {
+            if (err) {
+                x(err);
+            } else {
+                r(data);
+            }
+        });
+    });
+};
+
+exports.handler = async(event) => {
+
+    let params;
+    try {
+        params = await getParams();
+        console.log(params);
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
+
+    // //Refresh access token
+    const data = {
+        client_id: params.clientId,
+        client_secret: params.clientSecret,
+        grant_type: "refresh_token",
+        refresh_token: params.refreshToken,
+    };
+    const url = "https://www.strava.com/oauth/token?" + new URLSearchParams(data);
+
+    let res;
+    try {
+        res = await makeRequest(url, {});
+    } catch (e) {
+        console.error("Get token failed");
+        return false;
+    }
+
+    await setParam("/strava/refreshToken", res.refresh_token, "SecureString");
+    await setParam("/strava/accessToken", res.access_token, "SecureString");
+
+    params.refreshToken = res.refresh_token;
+    params.accessToken = res.access_token;
+
+    //Load activities
+    try {
+        const headers = {Authorization: `Bearer ${params.accessToken}`};
+        res = await makeRequest("https://www.strava.com/api/v3/athlete/activities", {}, "GET", headers);
+    } catch (e) {
+        return false;
+    }
+
+    const types = ["Run", "Hike", "Walk"];
+    const badShoes = ["g9558316"];
+
+    const needFixing = res.filter((act) => {
+        if (!types.includes(act.type)) {
+            return false;
+        }
+        return !act.gear_id || badShoes.includes(act.gear_id);
+    });
+
+    needFixing.forEach((act) => {
+        console.log(act.name, act.type, act.gear_id);
+    });
+
+    if (needFixing.length) {
+        const Message = needFixing.map((a) => `${a.name} (${a.type}) https://www.strava.com/activities/${a.id}`).join("\n");
+
+        const params = {
+            Message,
+            Subject: "Strava activities with missing footwear",
+            TopicArn: "arn:aws:sns:eu-west-1:574363388371:strava"
+        };
+        await publishSns(params);
+    }
+};
