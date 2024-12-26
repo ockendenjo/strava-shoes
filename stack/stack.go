6ac2071b8a2e87682037d2839cdae8d3267b3548
John Ockenden <29466887+ockendenjo@users.noreply.github.com>
2024-12-26 13:27:04 +0000

    Rename resource/table to allow Cfn replace

--- stack/stack.go
+++ stack/stack.go
@@ -39,12 +39,12 @@ func NewStack(scope constructs.Construct, id string, props *StackProps) awscdk.S
 		NoEcho:      jsii.Bool(true),
 	})
 
-	baggingDB := awsdynamodb.NewTableV2(stack, jsii.String("BaggingDB"), &awsdynamodb.TablePropsV2{
+	baggingDB := awsdynamodb.NewTableV2(stack, jsii.String("BaggingDBv2"), &awsdynamodb.TablePropsV2{
 		TableClass:          awsdynamodb.TableClass_STANDARD,
 		PartitionKey:        &awsdynamodb.Attribute{Type: awsdynamodb.AttributeType_STRING, Name: jsii.String("ID")},
 		Billing:             awsdynamodb.Billing_OnDemand(nil),
 		RemovalPolicy:       awscdk.RemovalPolicy_DESTROY,
-		TableName:           jsii.String("StravaShoesBagging"),
+		TableName:           jsii.String("StravaShoesBaggingv2"),
 		TimeToLiveAttribute: jsii.String("Expiry"),
 	})
 
